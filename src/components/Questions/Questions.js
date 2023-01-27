c6f97ff321d619fac2cecd3a210c2adc1889c693
Timelas <lom.a.s@icloud.com>
2023-01-27 14:46:46 +0300

    обнавление опроса

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -26,27 +26,38 @@ function Questions() {
             <span className="question__text-span">Нет</span>
           </label>
         </fieldset>
-        <fieldset className="question__string">Предпочтения по еде:
+        <fieldset className="question__string">Пожелания по напиткам:
           <label  htmlFor="chicken" className="question__label">
             <input className="question__unvisible" type="radio" name="eat" value="chicken" id="chicken"></input>
             <span className="question__visible-radio"></span>
-            <span className="question__text-span">Курица</span>
+            <span className="question__text-span">Шампанское</span>
           </label>
           <label htmlFor="fish" className="question__label">
             <input className="question__unvisible" type="radio" name="eat" value="fish" id="fish"></input>
             <span className="question__visible-radio"></span>
-            <span className="question__text-span">Рыба</span>
+            <span className="question__text-span">Вино красное полусладкое</span>
           </label>
           <label htmlFor="meat" className="question__label">
             <input className="question__unvisible" type="radio" name="eat" value="meat" id="meat"></input>
             <span className="question__visible-radio"></span>
-            <span className="question__text-span">Мясо</span>
+            <span className="question__text-span">Вино белое полусухое</span>
+          </label>
+          <label htmlFor="meat" className="question__label">
+            <input className="question__unvisible" type="radio" name="eat" value="meat" id="meat"></input>
+            <span className="question__visible-radio"></span>
+            <span className="question__text-span">Коньяк</span>
+          </label>
+          <label htmlFor="meat" className="question__label">
+            <input className="question__unvisible" type="radio" name="eat" value="meat" id="meat"></input>
+            <span className="question__visible-radio"></span>
+            <span className="question__text-span">Водка</span>
+          </label>
+          <label htmlFor="meat" className="question__label">
+            <input className="question__unvisible" type="radio" name="eat" value="meat" id="meat"></input>
+            <span className="question__visible-radio"></span>
+            <span className="question__text-span">Безалкогольные напитки</span>
           </label>
         </fieldset>
-        <p className="question__string">
-          <label htmlFor="alco" className="question__label-input">Пожелания по алкоголю:</label>
-          <input type="text" name="alco" id="alco" required className="question__input"></input>
-        </p>
         <button type="submit" className="question__button">Подтвердить</button>
       </form>
     </section>
