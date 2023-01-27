9e2b33d38af591857cfef3c0cd5caa2676ddcf58
Timelas <lom.a.s@icloud.com>
2023-01-27 13:43:05 +0300

    текстовые правки

--- src/components/DressCode/Dresscode.js
+++ src/components/DressCode/Dresscode.js
@@ -1,14 +1,11 @@
 import React from "react";
+import Title from "../Title/Title";
 import "./Dresscode.css";
 
 function Dresscode() {
   return (
     <section className="dresscode" id="dresscode">
-      <div className="dresscode__header">
-        <h2 className="dresscode__title">Dress code</h2>
-        <div className="dresscode__trait">Black Tie</div>
-      </div>
-      <div className="dresscode__figure"></div>
+      <Title title={"Dress Code"}/>
       <p className="dresscode__description">БУДЕМ ПРИЗНАТЕЛЬНЫ, ЕСЛИ ВЫ ПОДДЕРЖИТЕ ЦВЕТОВУЮ ГАММУ НАШЕГО ПРАЗДНИКА В СВОИХ НАРЯДАХ:</p>
       <div className="dresscode__colors">
         <div className="dresscode__color dresscode__color_var_one"></div>
