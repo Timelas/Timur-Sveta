e3d00c945c17128b28b9f1d39148ce55a2c26678
Timelas <lom.a.s@icloud.com>
2023-03-02 16:12:19 +0300

    правки невесты

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -8,7 +8,6 @@ function BurgerMenu(props) {
       <ul className="burger-menu__links">
       <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#home" className="burger-menu__ancor">ГЛАВНАЯ</a></li>
         <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#place" className="burger-menu__ancor">ГДЕ</a></li>
-        <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#dresscode" className="burger-menu__ancor">ДРЕСС КОД</a></li>
         <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#details" className="burger-menu__ancor">ДЕТАЛИ</a></li>
         <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#rsvp" className="burger-menu__ancor">RSVP</a></li>
         <li className={`burger-menu__link burger-menu__link_${isOpenBurger && 'visible'}`} onClick={onCloseBurger}><a href="#questions" className="burger-menu__ancor">ОПРОС</a></li>
